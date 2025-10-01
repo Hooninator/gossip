9c489350cc4c2663cb2f44700784d6293cc17fde
Julian Bellavita <jb2695@en-ci-cisugcl12..coecis.cornell.edu>
2025-10-01 14:29:17 -0400

    init

---
+++ src/main.py
@@ -0,0 +1,233 @@
+import socket
+import re
+import threading
+import argparse
+import time
+import random
+
+
+blacklist = []
+connections = {}
+local_digit = -1 
+myhost = "128.84.213.13"
+myport = "5678"
+myid = f"{myhost}:{myport}"
+msg_limit = 9000
+maxlines = 256
+conn_lock = threading.Lock()
+
+
+def pick_random_connection():
+    global connections
+
+    n = len(connections)
+
+    if n==0:
+        return "empty", ""
+
+    lc = list(connections)
+
+    while True:
+        idx = random.randint(0, n-1)
+        k = lc[idx]
+        if k != myid:
+            host, port = split_id(k)
+            return host, port
+        elif k==myid and n==1:
+            return "empty", ""
+
+
+def state_str():
+    global connections
+    s = ""
+    for addr in connections:
+        t,d = connections[addr]
+        if addr == myid and int(d) == -1:
+            continue
+        s += f"{addr},{t},{d}\n"
+    return s[:-1] # Exclude the last newline
+
+
+
+def open_conn(host, port):
+
+    hostport = f"{host}:{port}"
+
+    #if hostport in blacklist:
+    #    return
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            print("Valid connection")
+            s.connect((host, port))
+            print("Made connection")
+        except ConnectionRefusedError as e:
+            print(f"Could not connect to {hostport}. Error: {e}")
+            #print(f"Adding {hostport} to blacklist")
+
+            #conn_lock.acquire()
+
+            #blacklist.append(f"{hostport}")
+            #if hostport in connections:
+            #    del connections[hostport]
+
+            #conn_lock.release()
+
+            return
+        res = s.recv(msg_limit)
+        print(res)
+        parse_response(res)
+
+
+def parse_response(res):
+    global connections
+
+    res = res.decode()
+
+    if res == '':
+        return
+
+    lines = res.split("\n")
+
+    conn_lock.acquire()
+    i = 0
+    for line in lines:
+
+        if i > maxlines:
+            break
+
+        i += 1
+
+        s = line.split(",")
+        hostport, t, d = s
+
+
+        # Only update if newer timestamp
+        dold = 11
+        if hostport in connections:
+            told = connections[hostport][0]
+            dold = connections[hostport][1]
+            if told >= int(t) or int(time.time()) < int(t):
+                continue
+
+        connections[hostport] = (int(t), int(d))
+
+        if int(dold) != int(d):
+            print(f"{hostport} --> {d}")
+
+    conn_lock.release()
+
+
+
+def check_newconn(usr_input):
+    return re.match(r"^\+\d+(?:\.\d+){3}:\d+$", usr_input) is not None
+
+def check_update_digit(usr_input):
+    return re.match(r"\d$", usr_input)
+
+def check_question(usr_input):
+    return re.match(r"\?$", usr_input)
+
+def split_id(_id):
+    hostport = _id.split(":")
+    host, port = hostport[0], int(hostport[1])
+    return host, port
+
+def parse_input(usr_input):
+    global local_digit, connections
+
+    if check_newconn(usr_input):
+        # Case 1: Add new connection
+
+        inp = usr_input[1:]
+        host, port = split_id(inp)
+        print(host, port)
+        open_conn(host, port)
+
+
+    elif check_update_digit(usr_input):
+        # Case 2: Update local digit
+
+        conn_lock.acquire()
+        connections[myid] = (int(time.time()), int(usr_input))
+        conn_lock.release()
+
+        new_digit = int(usr_input)
+
+        if new_digit != local_digit:
+            local_digit = int(usr_input)
+            print(f"{myid} --> {local_digit}")
+
+
+
+    elif check_question(usr_input):
+        for connection in connections:
+            print(f"{connection} --> {connections[connection][1]}")
+
+    else:
+        # Invalid user input, do nothing
+        print(f"Invalid user input {usr_input}")
+
+
+def listen_loop():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+        # Open my socket
+        s.bind((myhost, int(myport)))
+        s.listen()
+
+        while True:
+            # Block until I receive a new connection
+            conn, addr = s.accept()
+
+            # Received new connection, send them a message
+            payload = state_str()
+            with conn:
+                conn.sendall(payload.encode())
+            
+
+def gossip_loop():
+    while True:
+
+        time.sleep(3.0)
+
+        host, port = pick_random_connection()
+
+        if host == "empty":
+            continue
+
+        open_conn(host, port)
+
+
+
+def hook():
+
+    # Listen for incoming connections
+    t1 = threading.Thread(target=listen_loop)
+
+    # Every 3 seconds, gossip with random node
+    t2 = threading.Thread(target=gossip_loop)
+
+    t1.start()
+    t2.start()
+
+    # Parse user input in a loop
+    while True:
+        usr_input = input()
+        parse_input(usr_input)
+        
+
+if __name__=="__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, required=True)
+    parser.add_argument("--port", type=str, required=True)
+    args = parser.parse_args()
+
+    myhost = args.host
+    myport = args.port
+    myid = f"{myhost}:{myport}"
+
+
+    hook()
+
